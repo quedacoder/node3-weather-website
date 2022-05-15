d9a4567a959229c2c921a4c8965fbb1b81a98009
quedacoder <quedacoder@gmail.com>
2022-05-15 11:36:43 -0500

    Removed unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,35 +1,33 @@
-console.log('Client side javascript file is loaded')
-
 // Get HTML elements my name
-const weatherForm = document.querySelector('form')
-const search = document.querySelector('input')
-const messageOne = document.querySelector('#message-one')
-const messageTwo = document.querySelector('#message-two')
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.querySelector("#message-one");
+const messageTwo = document.querySelector("#message-two");
 
 // add event listerner
-weatherForm.addEventListener('submit', (e) => {
-
+weatherForm.addEventListener("submit", (e) => {
     // stops the browser from reloading
-    e.preventDefault()
+    e.preventDefault();
 
     // get the value typed into the form
-    const location = search.value
+    const location = search.value;
 
-    messageOne.textContent = 'Loading....'
-    messageTwo.textContent = ''
+    messageOne.textContent = "Loading....";
+    messageTwo.textContent = "";
 
     // Calling api using fetch module
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
-        response.json().then((data) => {
-
-            if (data.error) {
-                messageOne.textContent = data.error
-                search.value = ''
-            } else {
-                messageOne.textContent = data.location
-                messageTwo.textContent = data.message.forecast
-                search.value = ''
-            }
-        })
-    })
-})
+    fetch("http://localhost:3000/weather?address=" + location).then(
+        (response) => {
+            response.json().then((data) => {
+                if (data.error) {
+                    messageOne.textContent = data.error;
+                    search.value = "";
+                } else {
+                    messageOne.textContent = data.location;
+                    messageTwo.textContent = data.message.forecast;
+                    search.value = "";
+                }
+            });
+        }
+    );
+});
